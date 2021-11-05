ad265c1fc3ec93c23c37c2f8ac5419c5dd4518fe
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-05 16:20:57 -0400

    All Tests Passinggit add ./Need to be cleaned up

--- api/task/model.js
+++ api/task/model.js
@@ -2,13 +2,33 @@
 const db = require('../../data/dbConfig')
 
 async function getAll() {
-    const result = await db('tasks')
-    const tasks = result.map((task) => {
+//     SELECT 
+//     t.*,
+//     p.project_name,
+//     p.project_description
+// FROM tasks as t
+// LEFT JOIN projects as p
+//     ON t.project_id = p.project_id
+// GROUP BY t.project_id
+// ORDER BY t.project_id ASC;
+
+const rows = await db('tasks as t')
+    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select('t.task_id', 
+        't.task_description', 
+        't.task_notes', 
+        't.task_completed',
+        'p.project_name', 
+        'p.project_description')
+
+    const tasks = rows.map((task) => {
         return {
             task_id: task.task_id,
             task_description: task.task_description,
             task_notes: task.task_notes,
-            task_completed: Boolean(task.task_completed)
+            task_completed: Boolean(task.task_completed),
+            project_name: task.project_name,
+            project_description: task.project_description
         }
     })
     return tasks
