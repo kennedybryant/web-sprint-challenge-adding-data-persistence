05cd8bbce1b9f62082397c12f15333cb3f632b69
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-05 12:14:29 -0400

    Setting Up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express')
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/project', projectRouter)
+
+server.use('/api/resource', resourceRouter)
+
+server.use('/api/task', taskRouter)
+
+server.use('*', (req, res) => {
+    res.json({ api: 'Up and running in server' })
+})
+
+module.exports = server
